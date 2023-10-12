9ff01b0c27ce5182da8ff2f5afe5de0dcd1c0ba6
Sahar Ali <saharnazimali@gmail.com>
2023-10-12 10:34:50 -0400

    Adds dispatch calls to App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Login from "./Login.js"
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
   
   // Run code based on a given condition
   useEffect(() => {
@@ -16,16 +18,37 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+      
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: 'SET_PLAYLISTS',
+          playlists: playlists
+        })
+      })
     }
 
     console.log('I HAVE A TOKEN: ', token);
   }, []);
+
   return (
     <div className="app">
       {
         token ? (
-          <h1>I am logged in</h1>
+          <Player spotify={spotify}/>
         ) : (
           <Login />
         )
