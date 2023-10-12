6a7f4f44824367dd9e484c16e1fb7ce90e21baee
Sahar Ali <saharnazimali@gmail.com>
2023-10-12 12:36:44 -0400

    Adds Footer that shows music controls and current song playing

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import { Grid, Slider } from '@mui/material';
 import "./Footer.css"
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
 
 function Footer() {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer">
+      <div className="footer__left">
+        <img 
+          className='footer__albumLogo'
+          src="https://upload.wikimedia.org/wikipedia/en/f/f7/Usher_-_Confessions_album_cover.png" 
+          alt=""
+        />
+        <div>
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green"></ShuffleIcon>
+        <SkipPreviousIcon className="footer__icon"></SkipPreviousIcon>
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon"></PlayCircleOutlineIcon>
+        <SkipNextIcon className="footer__icon"></SkipNextIcon>
+        <RepeatIcon className="footer__green"></RepeatIcon>
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
   )
 }
 
