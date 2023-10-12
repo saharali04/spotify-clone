c85c73898d62753d048bd8352f67ee400bf8e765
Sahar Ali <saharnazimali@gmail.com>
2023-10-12 08:19:22 -0400

    Stores token from URL in token state variable and removes it from URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login.js"
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+  
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log('I HAVE A TOKEN: ', token);
+  }, []);
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
